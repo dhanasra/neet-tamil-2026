35d06ed03d9807e3026c05a9ce4a435e17f508c0
Flutter Generator <actions@github.com>
2026-05-13 09:02:35 +0000

    Generator update - full_update

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:easy_localization/easy_localization.dart';
-import 'core/localization/localization_service.dart';
 import 'core/storage/storage_service.dart';
 
 import 'app/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
   await StorageService.init();
 
-  runApp(
-    EasyLocalization(
-      supportedLocales: LocalizationService.supportedLocales,
-      path: LocalizationService.path,
-      fallbackLocale: LocalizationService.fallbackLocale,
-      child: const App(),
-    ),
-  );
+  runApp(const App());
 }
